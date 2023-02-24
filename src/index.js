892809aabfec04a2cd39a71d77de1667891490c3
Paulo Fontenele <125470131+paulof5@users.noreply.github.com>
2023-02-24 08:54:53 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+//aqui estamos fazendo uma comparação de igualdade de valores
+console.log("(01)", "1" == 1);
+/*aqui estamos testando se os numeros são estritamente
+iguais, ou seja, se o valor e o tipo são os mesmos*/
+console.log("(02)", "1" === 1);
+//aqui estamos fazendo uma comparação de diferença de valores
+console.log("(03)", "1" != 1);
+/*aqui estamos testando se os numeros são extritamente
+diferentes, ou seja, além do valor também o tipo */
+console.log("(04)", "3" !== 3);
+//aqui vamos comparar se um número é maior que o outro
+console.log("(05)", 5 > 1);
+//aqui vamos comparar se um número é memnor que o outro
+console.log("(06)", 5 < 1);
+//aqui vamos comparar se um número é maior ou igual ao outro
+console.log("(07)", 5 >= 1);
+//aqui vamos comparar se um número é menor ou igual ao outro
+console.log("(08)", 5 <= 1);
